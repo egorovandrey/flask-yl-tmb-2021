9ac531ec6eefc0c76d8d6aa749ca8d16406541ae
egorovandrey <34152869+egorovandrey@users.noreply.github.com>
2022-03-15 16:54:14 +0300

    Update loginform.py

--- forms/loginform.py
+++ forms/loginform.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
-from wtforms.fields.html5 import EmailField
 
 
 class LoginForm(FlaskForm):
